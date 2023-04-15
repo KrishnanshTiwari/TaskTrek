c8ac1f649189d78896091b2b75f80cfd5a346b38
KrishnanshTiwari <128737843+KrishnanshTiwari@users.noreply.github.com>
2023-04-15 11:50:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 import Home from "./Home";
 export default function App() {
   return (
     <div>
-      <Routes>
-      <Route path="/" element={<Home />} />
       
-      </Routes>
+      <Home/>
       
     </div>
   );
